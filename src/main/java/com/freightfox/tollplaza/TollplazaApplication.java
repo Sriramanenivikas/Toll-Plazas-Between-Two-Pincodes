db318094f070fb3053950a094e4214af037e2df1
Sriramanenivikas <sriramanenivikas123@gmail.com>
2026-03-03 18:43:47 +0530

    Initial commit: Add toll plaza project

---
+++ src/main/java/com/freightfox/tollplaza/TollplazaApplication.java
@@ -0,0 +1,13 @@
+package com.freightfox.tollplaza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TollplazaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TollplazaApplication.class, args);
+    }
+
+}
